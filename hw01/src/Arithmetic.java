c34bad2c9b7f5cd9032472b1df44c3c30b4e82b3
Johndes123 <xionghongrui2005@gmail.com>
2026-05-04 08:26:48 +0800

    fixed Arithmetic.java in hw01

--- hw01/src/Arithmetic.java
+++ hw01/src/Arithmetic.java
@@ -19,8 +19,7 @@ public class Arithmetic {
      * @param b Value 2
      * @return Sum of a and b
      * */
-    public static int sum(int a, int b) {
-        return a * b;
+    public static int sum(int a, int b) {return a + b;
     }
 
     public static void main(String[] args) {
