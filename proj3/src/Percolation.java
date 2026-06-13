d6a9208c06976ffe2517f326d06c41e9d02fc1b3
Johndes123 <xionghongrui2005@gmail.com>
2026-06-13 18:13:41 +0800

    Completed Project 3

--- proj3/src/Percolation.java
+++ proj3/src/Percolation.java
@@ -1,37 +1,96 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
-    // TODO: Add any necessary instance variables.
 
+public class Percolation {
+
+    private WeightedQuickUnionUF ufP;
+    private WeightedQuickUnionUF ufF;
+    private int top;
+    private int bottom;
+    private int n;
+    private boolean[] open1;
+    private int opencount;
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if (N <= 0) throw new IllegalArgumentException();
+        ufP = new WeightedQuickUnionUF(N * N + 2);
+        ufF = new WeightedQuickUnionUF(N * N + 2);
+        n = N;
+        top = n * n;
+        bottom = n * n + 1;
+        open1 = new boolean[N * N];
+
+
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int idx = row * n + col;
+
+        if (!open1[idx]) {
+            open1[idx] = true;
+            opencount++;
+        } else {
+            return;
+        }
+
+        if (row > 0 && open1[idx - n]) {
+            ufP.union(idx, idx - n);
+            ufF.union(idx, idx - n);
+        }
+        if (row < n - 1 && open1[idx + n]) {
+            ufP.union(idx, idx + n);
+            ufF.union(idx, idx + n);
+        }
+        if (col > 0 && open1[idx - 1]) {
+            ufP.union(idx, idx - 1);
+            ufF.union(idx, idx - 1);
+        }
+        if (col < n - 1 && open1[idx + 1]) {
+            ufP.union(idx, idx + 1);
+            ufF.union(idx, idx + 1);
+        }
+        if (row == 0) {
+            ufP.union(idx, top );
+            ufF.union(idx, top);
+        }
+        if (row == n - 1) {
+            ufP.union(idx, bottom);
+        }
+
+
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int idx = row * n + col;
+        return open1[idx];
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if (row < 0 || row >= n || col < 0 || col >= n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int idx = row * n + col;
+        return ufF.find(idx) == ufF.find(top);
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return opencount;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+       return ufP.find(top) == ufP.find(bottom);
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
     // TODO: Remove all TODO comments before submitting.
 
 }
+
